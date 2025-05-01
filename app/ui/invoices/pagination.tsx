c87432a14531a8a1532abd4bcd9eb61799d56c64
Ibe Arua <stanarua@aol.com>
2025-05-01 21:26:59 +0100

    adding other components for invoices

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -3,7 +3,7 @@
 import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
-import { generatePagination } from '@/app/lib/utils';
+// import { generatePagination } from '@/app/lib/utils';
 import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
@@ -12,13 +12,13 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   // const allPages = generatePagination(currentPage, totalPages);
   const pathname = usePathname();
   const searchParams = useSearchParams();
-  const currentPage = Number(searchParams.get('page')) || 1;
+  // const currentPage = Number(searchParams.get('page')) || 1;
 
-  const createPageURL = (pageNumber: number | string) => {
-    const params = new URLSearchParams(searchParams);
-    params.set('page', pageNumber.toString());
-    return `${pathname}?${params.toString()}`;
-  };
+  //   const createPageURL = (pageNumber: number | string) => {
+  //   const params = new URLSearchParams(searchParams);
+  //   params.set('page', pageNumber.toString());
+  //   return `${pathname}?${params.toString()}`;
+  // };
  
 
   return (
